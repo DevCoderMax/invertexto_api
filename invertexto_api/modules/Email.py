7c9800a9bfe1fbbadf5709496aab53ec515e823c
luisalvs <luisalvst@gmail.com>
2024-11-08 21:15:41 -0300

    Update Email.py

--- invertexto_api/modules/Email.py
+++ invertexto_api/modules/Email.py
@@ -7,10 +7,10 @@ class Email:
         self._base_url = "https://www.invertexto.com/gerador-email-temporario?email="
         self._end_url = "@uorak.com"
 
-def generate_email():
-    page = requests.get("https://www.invertexto.com/gerador-email-temporario").content
-    soup = BeautifulSoup(page, 'html.parser') 
-    email = soup.find(id='email-input') # pega o e-mail pelo elemento id
-    return email.get('value') # retorna o e-mail
+    def generate_email():
+        page = requests.get("https://www.invertexto.com/gerador-email-temporario").content
+        soup = BeautifulSoup(page, 'html.parser') 
+        email = soup.find(id='email-input') # pega o e-mail pelo elemento id
+        return email.get('value') # retorna o e-mail
 
-generate_email()
+    generate_email()
