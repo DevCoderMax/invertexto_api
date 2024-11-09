6375936afefdd74eaaa5720e9f6fd7d769b3885e
BIG4O <michaelnatanael27@gmail.com>
2024-11-09 09:24:23 -0300

    Update Email.py

--- invertexto_api/modules/Email.py
+++ invertexto_api/modules/Email.py
@@ -12,3 +12,17 @@ class Email:
         soup = BeautifulSoup(page, 'html.parser') 
         email = soup.find(id='email-input') 
         return email.get('value') 
+
+    def create_email(username):
+    
+        url = "https://www.invertexto.com/gerador-email-temporario?email="
+        end_url = "@uorak.com"
+        url_final = url+username+end_url
+        email = username+end_url
+        response = requests.get(url_final)
+
+        if email in response.text:
+            return True
+        else:
+            return False
+        
